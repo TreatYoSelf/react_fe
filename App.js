c362789c4c2f55d943622d5c7169d6c081afbaab
Joel Lacey <joellaceybusiness@gmail.com>
2020-04-02 19:01:22 -0600

    Alter App to utilize Landing component

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Landing from './src/components/Landing/Landing';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Landing />
     </View>
   );
 }
