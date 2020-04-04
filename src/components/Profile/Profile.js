1cd04ca7d047a11cc9ba5c3090236f5b257d24b0
Joel Lacey <joellaceybusiness@gmail.com>
2020-04-04 17:09:38 -0600

    Create Profile component

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, Image, View, Picker } from 'react-native';
+import { mockTreat } from '../../mockData/mockTreat';
+
+export default function Profile() {
+    const {icon, title, rating: defaultRating} = mockTreat;
+    const [rating, setRating] = useState(defaultRating);
+
+    console.log(icon)
+    return (
+        <View style={styles.container}>
+            <Image source={require("../../../assets/logo.png")} style={{ width: 200, height: 200 }} />
+            <Text>{title}</Text>
+            <Picker
+                selectedValue={rating}
+                style={{ height: 50, width: 150 }}
+                onValueChange={(itemValue, itemIndex) => setRating(itemValue)}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 1,
+        height: 30,
+        width: 100
+    },
+});
