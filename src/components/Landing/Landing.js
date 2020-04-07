933053e664bbd7a139b681df95775c3e30d6ffe9
Joel Lacey <joellaceybusiness@gmail.com>
2020-04-07 16:56:53 -0600

    Modify Landing to eventually handle login

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, Image, View } from 'react-native';
-// import { Link } from "react-router-native";
+import { StyleSheet, Text, Image, View, Button } from 'react-native';
 
-export default function Landing() {
+export default function Landing(props) {
     return (
-        <View style={styles.container}>
+        <View>
             <Image source={require("../../../assets/logo.png")} style={{ width: 200, height: 200 }} />
             <Text>An app where you can treat yourself to some personal time, guilt free.</Text>
-            <Link to="/profile"><Text>Profile</Text></Link>
+            <Text style={styles.header}>Sign In With Google</Text>
+            <Button title="Sign in with Google" onPress={() => props.signIn()} />
         </View>
     );
 }
@@ -15,10 +15,20 @@ export default function Landing() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
     },
-});
-
+    header: {
+        fontSize: 25
+    },
+    image: {
+        marginTop: 15,
+        width: 150,
+        height: 150,
+        borderColor: "rgba(0,0,0,0.2)",
+        borderWidth: 3,
+        borderRadius: 150
+    }
+})
 // export default Landing;
