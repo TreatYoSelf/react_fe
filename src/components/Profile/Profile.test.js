7f5a55d20e25f99a5497f134db4224e2e5c108e5
Joel Lacey <joellaceybusiness@gmail.com>
2020-04-11 11:43:09 -0600

    Update component tests for Apollo

--- src/components/Profile/Profile.test.js
+++ src/components/Profile/Profile.test.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { MockedProvider } from '@apollo/react-testing';
 
 import Profile from './Profile';
 
 describe('<Profile />', () => {
+    let tree;
+
+    beforeEach(() => {
+        tree = renderer.create(
+            <MockedProvider addTypename={false}>
+                 <Profile />
+            </MockedProvider>
+        ).toJSON();
+    })
+
     it('has 3 children', () => {
-        const tree = renderer.create(<Profile />).toJSON();
         expect(tree.children.length).toBe(3);
     });
 
     it('renders correctly', () => {
-        const tree = renderer.create(<Profile />).toJSON();
         expect(tree).toMatchSnapshot();
     });
 });
