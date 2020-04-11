30ba3084c4b5bcdb6e07df859ab8548e26268505
Wren Steitle <lauren.steitle@gmail.com>
2020-04-11 14:27:07 -0600

    Add nav bar items file

---
+++ src/components/NavBar/NavBarItems.js
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+  AsyncStorage,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Image
+} from "react-native";
+import Colors from "../constants/Colors";
+import { useNavigation } from "@react-navigation/native";
+
+export default function NavBarItems({ closeMenu, action }) {
+  const navigation = useNavigation();
+  const handlePress = path => {
+    navigation.navigate(path, { action });
+    closeMenu();
+  };
+
+  const handleLogout = () => {
+    try {
+      AsyncStorage.removeItem("action").then(() =>
+        navigation.navigate("Login")
+      );
+    } catch (exception) {
+      console.log("Error clearing storage");
+    }
+  };
+
+  return (
+    <View style={styles.drawerItems}>
+      <TouchableOpacity onPress={() => handlePress("Path")}>
+        <Text style={styles.drawerText}>
+          Go to {action === "borrow" ? "Lending" : "Borrowing"}
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => handlePress("My Items", { action: action })}
+      >
+        <Text style={styles.drawerText}>
+          View My {action === "borrow" ? "Borrowed" : "Loaned"} Items
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => handlePress("RequestScreen")}>
+        <Text style={styles.drawerText}>
+          {action === "borrow" ? "Make a Request" : "Loan an Item"}
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => handleLogout()}>
+        <Text style={styles.drawerText}>Logout</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  drawerItems: {
+    backgroundColor: Colors.lightBlue,
+    height: 280,
+    width: 300
+  },
+  drawerText: {
+    borderColor: "#fff",
+    borderWidth: 1,
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+    height: 70,
+    padding: 20,
+    width: "100%"
+  }
+});
