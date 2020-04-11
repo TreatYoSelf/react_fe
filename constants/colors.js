6b7456fd2022fe99318c89fd0abcc20e71517fd3
Wren Steitle <lauren.steitle@gmail.com>
2020-04-11 14:26:25 -0600

    Color file needs to be updated with correct colors

---
+++ constants/colors.js
@@ -0,0 +1,17 @@
+const tintColor = '#0C94E1';
+
+export default {
+  tintColor,
+  darkBlue: '#385A94',
+  lightBlue: '#0C94E1',
+  aqua: '#0CE1C3',
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+};
