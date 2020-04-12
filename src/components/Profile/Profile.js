f02ed36158a14caa16d14cd3d3c9974e0a53b2b6
Joel Lacey <joellaceybusiness@gmail.com>
2020-04-12 12:17:32 -0600

    Localize category query to Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,29 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, Image, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, Image, View, ScrollView, FlatList } from 'react-native';
 import SuggestedTreat from '../../containers/suggestedTreat/SuggestedTreat';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
 export default function Profile(props) {
     //need to fetch all activities for a user, and map over them adding suggesteTreats for each
     //temporarily load SuggestedTreat
+    let catDisplay = [];
+    const myQuery = gql`
+        {
+          getCategories {
+            name
+          }
+        }
+      `;
+
+    const { error, data } = useQuery(myQuery, { errorPolicy: 'all' });
+    // if (error) {
+    //     setErrors(error)
+    // }
+    if (data) {
+        catDisplay = data.getCategories.map((category, i) =>
+        <SuggestedTreat key={i} title={category.name} />)
+    }
+    //need to error display
+  
+    // console.log('hit', categories)
+
 
     return (
         <View style={styles.container}>
             <Text style={styles.header}>Welcome:{props.name}</Text>
             <Image style={styles.image} source={{ uri: props.photoUrl }} />
-            <SuggestedTreat />
+            <ScrollView style={styles.categories}>
+                {catDisplay}
+            </ScrollView>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         backgroundColor: "#fff",
         alignItems: "center",
-        justifyContent: "center"
+        // justifyContent: "space-between",
+        // height: 40,
+        // width: 100
     },
     header: {
-        fontSize: 25
+        fontSize: 25,
     },
     image: {
         marginTop: 15,
@@ -32,5 +58,11 @@ const styles = StyleSheet.create({
         borderColor: "rgba(0,0,0,0.2)",
         borderWidth: 3,
         borderRadius: 150
+    },
+    categories: {
+        backgroundColor: "#fff",
+        flexGrow: 0,
+        // height: 60
+        // width: 100
     }
 })
