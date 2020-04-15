03f094d4061e2bc941a96b4aa0e5ad3224c7afb8
Joel Lacey <joellaceybusiness@gmail.com>
2020-04-15 12:49:47 -0600

    Add mock user data

--- src/mockData/mockTreat.js
+++ src/mockData/mockTreat.js
@@ -64,5 +64,19 @@ export const mockEvents = [{
             "eventEndTime": "1396682309130.185"
     }];
 
+export const mockUser = {
+    accessToken: "ya29.a0Ae4lvC1pqc5Cba23lW-FzAtzOJyQ9SQCWmy0ieLjCWn-8p4XZ65Ba8SLRmPIhDEGyv38kfvkT_o3uevsJ2HRUlKTvEbyibGDziIIGhUCa31zDVUUlc23o0tdp3F6c5erDMKSCDUAzzczq9N87_hZDjI6adiSMl_TQoM",
+    idToken: "eyJhbGciOiJSUzI1NiIsImtpZCI6IjZmY2Y0MTMyMjQ3NjUxNTZiNDg3NjhhNDJmYWMwNjQ5NmEzMGZmNWEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI4NDI0ODY4MjkyNDItbXEwZTZkNWxwc3FsN2w5dTQxbzRjY2xhcXBycHNianAuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI4NDI0ODY4MjkyNDItbXEwZTZkNWxwc3FsN2w5dTQxbzRjY2xhcXBycHNianAuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTgxMjM5MDQ3MDMzNTY3NDA1NTQiLCJlbWFpbCI6ImxhY2V5ZmFtaWx5MjAzQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiNGdpMzVuZl9aazdhR3VLTHJSYnRhQSIsIm5hbWUiOiJMYWNleSBGYW1pbHkiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDQuZ29vZ2xldXNlcmNvbnRlbnQuY29tLy16QkNva183Qk5wMC9BQUFBQUFBQUFBSS9BQUFBQUFBQUFBQS9BQUtXSkpQMVF3eEs3V0x4MGhRLXZZY0lId1k5dV9wbVJRL3M5Ni1jL3Bob3RvLmpwZyIsImdpdmVuX25hbWUiOiJMYWNleSIsImZhbWlseV9uYW1lIjoiRmFtaWx5IiwibG9jYWxlIjoiZW4iLCJpYXQiOjE1ODY5Njc3MTAsImV4cCI6MTU4Njk3MTMxMH0.GLKpXoTmPPIs_oFh3mmJNNY9XmosgJEqwmYSY3LZ_A_u4woypvbihx5sEiaYvRSbdKKxIs0-ZKXE6CryQtFaJZGqXF7C9iA7tyyX3P4Ueu2T1REbV0MJni_St2-XOmwARV-zDCvfLSvet9jfMKaH2Sfb0tlle3lbqwMfLc73qIvswsWwJGdzl42c8weBzuh2z-1g935WNfT_iOhDmpR3iO96KIkE1S2DjoT-5Vgr0TxfSqXQlP6EjMQVRJ1oeYGfbRpipPEMvNncaqZLCA6z4pOmo8ZE4JN57OKXGEGd8ASx4S3fHNyVfvXTfHBeOe7jp3oFfF6wnEGYngsDIFUnvA",
+    refreshToken: "1//04LkubD0qny1SCgYIARAAGAQSNwF-L9IryGTjtm1F7Y9m1NWLQLeWjwClB2yH70wy7lUBqYVhOyNPPSqpNp3CIbY2Z5K1bCymKRs",
+    type: "success",
+    user: {
+        email: "laceyfamily203@gmail.com",
+        familyName: "Family",
+        givenName: "Lacey",
+        id: 118123904703356740554,
+        name: "Lacey Family",
+        photoUrl: "https://lh4.googleusercontent.com/-zBCok_7BNp0/AAAAAAAAAAI/AAAAAAAAAAA/AAKWJJP1QwxK7WLx0hQ-vYcIHwY9u_pmRQ/photo.jpg",
+    },
+}
 //have one task that's scheduled
 //have one task that's just a potential 
