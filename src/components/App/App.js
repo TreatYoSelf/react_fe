4d418ef3abebba697b2c3365fda9d369809dddd0
Joel Lacey <joellaceybusiness@gmail.com>
2020-04-15 12:48:44 -0600

    Add additional app route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,13 +6,16 @@ import Login from '../../containers/Login/Login';
 import { Route } from "react-router-native";
 import { NativeRouter } from "react-router-native";
 import Calendar from '../Calendar/Calendar';
+import Profile from '../Profile/Profile';
+import PreferenceForm from '../../containers/PreferenceForm/PreferenceForm';
 
 export default function App() {  
   return (
     <NativeRouter>
       <View style={styles.container}>
         <Route exact path="/" component={Login} />
-        {/* <Route exact path="/profile" component={Profile} />  */}
+        <Route exact path="/profile" component={Profile} /> 
+        <Route exact path="/calendar" component={Calendar} /> 
       </View>
     </NativeRouter>
   );
