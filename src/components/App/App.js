25391ee7ea139585ec9537b27f58dcc3ea157a2e
Joel Lacey <joellaceybusiness@gmail.com>
2020-04-16 15:38:38 -0600

    Remove unused app references

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-// import Landing from './src/components/Landing/Landing';
-// import Profile from './src/components/Profile/Profile';
 import Login from '../../containers/Login/Login';
 import { Route } from "react-router-native";
 import { NativeRouter } from "react-router-native";
 import Calendar from '../Calendar/Calendar';
 import Profile from '../Profile/Profile';
-import PreferenceForm from '../../containers/PreferenceForm/PreferenceForm';
 
 export default function App() {  
   return (
@@ -27,7 +24,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#b6bfac',
-    // alignItems: 'center',
     justifyContent: 'center',
     flexDirection: "column",
     height: "100%",
